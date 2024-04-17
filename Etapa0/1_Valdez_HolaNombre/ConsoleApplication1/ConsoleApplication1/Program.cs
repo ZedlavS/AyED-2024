9fedae394f70cd0f6596ffb98e69a9e06493c375
ZedlavS <167351480+ZedlavS@users.noreply.github.com>
2024-04-17 16:53:11 -0300

    Subo etapa 0

---
+++ Etapa0/1_Valdez_HolaNombre/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            Console.WriteLine("¿Cual es su nombre?");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Hola " + nombre);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Etapa0/2_Valdez_TextVariables/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadena;
+            int entero;
+            bool bolean;
+            float flotante;
+            double doble;
+            char caracter;
+
+            cadena = "15.557294";
+            entero = 15;
+            bolean = false;
+            flotante = 15.557294f;
+            doble = 15.557294;
+            caracter = '1';
+
+            Console.WriteLine("string " + cadena);
+            Console.WriteLine("int " + entero);
+            Console.WriteLine("bool " + bolean);
+            Console.WriteLine("float " + flotante);
+            Console.WriteLine("double " + doble);
+            Console.WriteLine("char " + caracter);
+
+            entero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(entero);
+
+            Console.ReadKey();
+
+        }
+    }
+}
