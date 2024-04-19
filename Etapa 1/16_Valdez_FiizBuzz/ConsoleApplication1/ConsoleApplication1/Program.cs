89431de8792f814c2cc21334d63e1fcdd31c985d
ZedlavS <167351480+ZedlavS@users.noreply.github.com>
2024-04-19 16:08:27 -0300

    Subo 16-17

---
+++ Etapa 1/16_Valdez_FiizBuzz/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            while (i < 100)
+            {
+                
+
+                if (i % 3 == 0 & i % 5 == 0) 
+                {   
+                    i++;                   
+                    Console.WriteLine("FizzBuzz");
+                    
+                }else if (i % 3 == 0 & i % 5 != 0)
+                {
+                    i++;
+                    Console.WriteLine("Fizz");
+                }else if (i % 5 == 0 & i % 3 != 0)
+                {
+                    i++;
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                    i++;
+                }
+                
+                
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
