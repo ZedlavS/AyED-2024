d5d54a69388d0a735b5f2a621b46bb0c0c8b8627
ZedlavS <167351480+ZedlavS@users.noreply.github.com>
2024-04-24 17:00:03 -0300

    subo ejercicios 18-19

---
+++ Etapa 1/18_Valdez_HastaQueSi/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string si;
+            bool para = true;
+            while (para == true)
+            {
+                Console.WriteLine("deseseas terminar el programa");
+                si = Console.ReadLine();
+                if (si == "Si")
+                {
+                    para =false;
+                    Console.WriteLine("chau");
+                }
+            }Console.ReadKey();
+        }   
+    }
+}
---
+++ Etapa 1/19_Valdez_HastaN/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            bool para = true;
+            Console.WriteLine("numero?");
+            num = int.Parse(Console.ReadLine());
+            int i = 0;
+            while (i<num)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+
+                }
+                i++;
+            }Console.ReadKey();
+
+        }
+    }
+}
