2ba665d666e9a151a7d52e75aa4ac7fb99b4f282
ZedlavS <167351480+ZedlavS@users.noreply.github.com>
2024-05-03 16:07:44 -0300

    subo ejercicios 1-2 etapa 2

---
+++ Etapa2/1_Valdez_LaFiestaDeStitch/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int com;
+
+            int total = 0;
+
+            bool para = true;
+
+            Console.WriteLine("cuantos invitados hay?");
+
+            num = int.Parse(Console.ReadLine());
+
+
+            int[] invits = new int[num];
+
+            Console.Clear();
+
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine("cuanto come el invitado " + i);
+                com = int.Parse(Console.ReadLine());
+                while (com < 0 || com > 100)
+                {
+                    Console.WriteLine("tiene que ser entre 0 y 100");
+                    com = int.Parse(Console.ReadLine());
+                }
+                invits[i] = com;
+                total = total + invits[i];
+
+            }
+           
+             Console.WriteLine("come un promedio de = " + (total / num));
+
+            Console.ReadKey();
+            
+
+
+
+
+        }
+    }
+}
---
+++ Etapa2/2_Valdez_ElRayoCarreraVeloz/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+            int prom = 0;
+
+            int vuelta;
+            int segundo;
+
+            int mayor1;
+
+            int mayor = 0;
+
+
+            Console.WriteLine("cuantas vueltas da el rayo");
+            vuelta = int.Parse(Console.ReadLine());
+
+            int[] vueltas = new int[vuelta];
+
+            for (int i = 0; i < vuelta; i++)
+            {
+                Console.WriteLine("cuanto tardo en la vuelta " + i);
+                segundo = int.Parse(Console.ReadLine());
+
+                vueltas[i] = segundo;
+
+            }
+            for (int i = 0; i < vuelta; i++)
+            {
+                total = vueltas[i] + total;
+
+
+            }
+
+            mayor1 = vueltas[0];
+
+            for (int i = 1; i < vuelta; i++)
+            {
+
+
+                if (mayor1 > vueltas[i])
+                {
+                    mayor = vueltas[i];
+                    mayor1 = vueltas[i];
+
+                }
+                else
+                {
+                    mayor = mayor1;
+                }
+
+            }
+
+
+            prom = total / vuelta;
+            Console.Clear();
+            Console.WriteLine("tardo un total de " + total);
+            Console.WriteLine("el promedio de tiempo es " + prom);
+                Console.WriteLine("la mejor vuelta es de "+mayor);
+                
+                Console.ReadKey();
+
+
+            }
+        }
+    }
+
