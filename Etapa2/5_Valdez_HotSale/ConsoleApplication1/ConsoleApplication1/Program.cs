5472fa2208b21cd93c76450ec060759f2b27f2f7
ZedlavS <167351480+ZedlavS@users.noreply.github.com>
2024-05-17 16:51:59 -0300

    subo ejercicio -6

---
+++ Etapa2/5_Valdez_HotSale/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int vendido;
+
+            Console.WriteLine("cuanto productos se vendieron ");
+            vendido = int.Parse(Console.ReadLine());
+
+            int[] productos = new int[vendido];
+
+            for (int i = 0; i < vendido; i++)
+            {
+                Console.WriteLine("cuanto costo el producto numero " + (i + 1));
+                productos[i] = int.Parse(Console.ReadLine());
+            }
+
+            int mayor = productos[0];
+            int menor = mayor;
+
+            for (int i = 1; i < vendido; i++)
+            {
+                if (mayor < productos[i])
+                {
+                    mayor = productos[i];
+                }
+                else
+                {
+                    menor = productos[i];
+                }
+            }
+            Console.WriteLine("el produto mayor vendido fue de " + mayor);
+            Console.WriteLine("el produto menor vendido fue de " + menor);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Etapa2/6_Valdez_ZEGARPG/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int objetos;
+            int filtro;
+
+            Console.WriteLine("cuantos objetos deseas analizar ");
+            objetos = int.Parse(Console.ReadLine());
+
+            int[] elementos = new int[objetos];
+
+            Console.WriteLine("di el valor nminimo para que los objetos entren en el filtro");
+            filtro = int.Parse(Console.ReadLine());
+
+            for ( int i = 0; i < objetos; i++)
+            {
+                Console.WriteLine("dime el valor del objeto numero " + (i + 1));
+                elementos[i] = int.Parse(Console.ReadLine());
+            }
+
+           
+
+            for ( int i= 0; i < objetos; i++)
+            {
+                if (filtro < elementos[i])
+                {
+                    Console.WriteLine("el objeto en la posicion numero " + (i + 1) + " paso el filtro");
+
+                }
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
