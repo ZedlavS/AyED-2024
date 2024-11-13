13ddcdec3a35e00eb600ba436a082541559c87f0
ZedlavS <167351480+ZedlavS@users.noreply.github.com>
2024-11-13 20:24:37 -0300

    Add files via upload

---
+++ Etapa2/10_Valdez_MatrizAleatoria/MatrizAleatoria/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrizAleatoria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, m;
+
+            Console.WriteLine("columnas");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("filas");
+            m = int.Parse(Console.ReadLine());
+
+
+            int[,] matriz = new int[n, m];
+
+            Random rng = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matriz[i, j] = rng.Next(1,100);
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(matriz[i, j] + "\t");
+                }
+                Console.WriteLine("");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Etapa2/9_Valdez_MiPrimerMatriz/ConsoleApplication1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, m;
+
+            Console.WriteLine("columnas");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("filas");
+            m = int.Parse(Console.ReadLine());
+            int suma = 0;
+
+            int[,] matriz = new int[n, m];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matriz[i, j] = suma;
+                    suma++;
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(matriz[i, j]+ "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
