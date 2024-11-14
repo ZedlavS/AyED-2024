aae3d9516a329dbdea105461cf5a505f908326c8
ZedlavS <167351480+ZedlavS@users.noreply.github.com>
2024-11-14 18:34:12 -0300

    13-14

---
+++ Etapa2/13_Valdez_ObtenerDiagonalPrincipal/ConsoleApplication1/Program.cs
@@ -0,0 +1,49 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.Write("Indicar el tamaño del vector y de la matriz (solo un número): ");
+            n = int.Parse(Console.ReadLine());
+            int[,] matriz = new int[n, n];
+            int[] vector = new int[n];
+            Random rango = new Random();
+
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    matriz[i, j] = rango.Next(1, 10);
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(matriz[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("---------------------------------------");
+            for (int i = 0; i < n; i++)
+            {
+                vector[i] = matriz[i, i];
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(vector[i] + "\t");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Etapa2/14_Valdez_CalificacionesEstudiantiles/ConsoleApplication1/Program.cs
@@ -0,0 +1,41 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int est;
+
+            Console.Write("Ingrese la cantidad de estudiantes: ");
+            est = int.Parse(Console.ReadLine());
+            Console.Write("Colocar el nombre, edad y calificacion (en ese orden): ");
+            Console.WriteLine();
+            string[,] matriz = new string[3, est];
+            for (int i = 0; i < est; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    matriz[j, i] = Console.ReadLine();
+
+                    Console.Clear();
+                }
+                Console.WriteLine();
+            }
+            for (int i = 0; i < est; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(matriz[j, i] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
